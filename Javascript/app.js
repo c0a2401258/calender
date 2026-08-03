1beec0726535aacc0dfd346af30f1958d8ab072f
c0a2401258 <c0a2401258@edu.teu.ac.jp>
2026-08-03 23:12:07 +0900

    ファイル構成の整理

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,10 +1,44 @@
-// =================================
-// アプリ全体
-// 担当：C
-// =================================
+// 表示先
+const app = document.getElementById("app");
 
-// 初期処理
+// 共通の画面読み込み関数
+async function loadPage(page) {
 
-// 画面切り替え
+    try {
 
-// 起動処理
+        // HTMLファイルを取得
+        const response = await fetch(page);
+
+        // 文字列として取得
+        const html = await response.text();
+
+        // 画面へ表示
+        app.innerHTML = html;
+
+    } catch (error) {
+
+        app.innerHTML = "<h2>ページを読み込めませんでした</h2>";
+        console.error(error);
+
+    }
+}
+
+// ログイン画面
+function showLogin() {
+    loadPage("pages/login.html");
+}
+
+// メイン画面
+function showCalendar() {
+    loadPage("pages/calendar.html");
+}
+
+// 日ごとの予定画面
+function showSchedule() {
+    loadPage("pages/schedule.html");
+}
+
+// アプリ起動時
+window.onload = function () {
+    showLogin();
+};
