ff6763fad9befb95f9170155cbed0db2aaf01365
c0a2401258 <c0a2401258@edu.teu.ac.jp>
2026-08-04 10:42:46 +0900

    db.php完成

--- PHP/db.php
+++ PHP/db.php
@@ -4,3 +4,17 @@
 担当：C
 ================================= 
 -->
+<?php
+
+$host="localhost";  //接続先
+$user="root";  //ログインユーザ名（XAMPPならroot）
+$password="";
+$dbname="calendar_db";  //データベース名
+
+$conn=new mysqli($host,$user,$password,$dbname);  //接続開始
+
+if($conn->connect_error){
+    die("接続失敗");  //終了
+}
+
+?>
