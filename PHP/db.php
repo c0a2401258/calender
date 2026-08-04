8dfb41b24fe32386747fea7efd69659a3e547e68
c0a2401258 <c0a2401258@edu.teu.ac.jp>
2026-08-04 10:48:31 +0900

    db.php修正

--- PHP/db.php
+++ PHP/db.php
@@ -14,7 +14,10 @@ $dbname="calendar_db";  //データベース名
 $conn=new mysqli($host,$user,$password,$dbname);  //接続開始
 
 if($conn->connect_error){
-    die("接続失敗");  //終了
+//    die("接続失敗");  //終了
+    die("接続失敗: " . $conn->connect_error);  //終了
 }
+
+$conn->set_charset("utf8mb4");  //文字コード設定
 
 ?>
